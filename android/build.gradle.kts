3422beda543e9ad642859e8526e56c9eae18c5a6
Danilo <dan.vasconcelos76@gmail.com>
2026-05-28 18:56:34 -0300

    iniciando teste mobiles

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,21 +12,6 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    afterEvaluate { project ->
-        if (project.extensions.findByName("android") != null) {
-            @Suppress("UnstableApiUsage")
-            project.extensions.configure("android") {
-                compileSdk 34
-                buildToolsVersion "34.0.0"
-                
-                defaultConfig {
-                    targetSdk 34
-                }
-            }
-        }
-    }
-}
-subprojects {
     project.evaluationDependsOn(":app")
 }
 
